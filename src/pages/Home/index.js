aeb07cab3da39bfe11e4ee582a22b5e7f6bca9ad
Rafael Langoni <langoninaves@gmail.com>
2022-08-04 17:37:51 -0300

    funcionando ok a chamada a api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,44 +6,31 @@ import { api_key } from "../../config/key"
 
 const Home = () => {
 
-    // const [filmes, setFilmes] = useState([]);
-    // const image_path = 'https://image.tmdb.org/t/p/w500'
+    const [movies, setMovies] = useState([]);
+    const image_path = 'https://image.tmdb.org/t/p/w500'
 
-    // useEffect(() => {
-    //     fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`)
-    //         .then(response => response.json)
-    //         .then(data => {
-    //             setFilmes(data)
-    //             console.log(`conteudo filmes ${filmes}`)
-    //         })
-    //         .catch((error) => {
-    //             console.log(error.message);
-    //         })
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`)
+            .then(response => response.json())
+            .then(data => {
+                setMovies(data.results)
+                console.log(data.results)
+            })
+            .catch((error) => {
+                console.log(error.message);
+            })
     
-    // })
-
-    const filmes = [
-        {
-            id: 1,
-            title: "Spider",
-            url: 'https://midias.correiobraziliense.com.br/_midias/jpg/2021/11/11/1200x800/1_poster_de_homem_aranha_sem_volta_para_casa_foto_reproducaomarvel_studios_widelg-7045997.jpg?20211111102937?20211111102937'
-        },
-        {
-            id: 2,
-            title: "Spider-Man",
-            url: 'https://midias.correiobraziliense.com.br/_midias/jpg/2021/11/11/1200x800/1_poster_de_homem_aranha_sem_volta_para_casa_foto_reproducaomarvel_studios_widelg-7045997.jpg?20211111102937?20211111102937'
-        }
-    ]
+    }, [])
 
     return (
         <Container>
             <h1>Filmes</h1>
             <ListaFilmes>
-                {filmes.map(filme => {
+                {movies.map(movie => {
                     return (
-                    <Filme key={filme.id}>
-                        <a href="https://www.google.com.br"><img src={filme.url} alt={filme.title}></img></a>
-                        <span>{filme.title}</span>
+                    <Filme key={movie.id}>
+                        <a href="https://www.google.com.br"><img src={`${image_path}${movie.poster_path}`} alt={movie.title}></img></a>
+                        <span>{movie.title}</span>
                     </Filme>
                     )
                 })}
